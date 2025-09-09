03cb6e5b28bf5eec37731a8963906f2aa285e794
Esteban Munoz <121775911+esteban24munoz@users.noreply.github.com>
2025-09-09 14:40:18 -0500

    fixed deploy errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
 import Header from "./layout/header";
 import Carmen from "./pages/honduras";
 import LandingPage from "./pages/landing";
@@ -6,17 +7,26 @@ import Haydee from "./pages/salvador";
 import Wendy from "./pages/guatemala";
 
 function App() {
+  // Handle GitHub Pages redirect
+  useEffect(() => {
+    const path = window.location.pathname;
+    if (path.includes("/?/")) {
+      const newPath = path.replace("/?/", "/");
+      window.history.replaceState({}, "", newPath);
+    }
+  }, []);
+
   return (
     <>
-        <Router>
-          <Header />
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/honduras" element={<Carmen />} />
-            <Route path="/salvador" element={<Haydee />} />
-            <Route path="/guatemala" element={<Wendy />} />
-          </Routes>
-        </Router>
+      <Router basename="/foodprints-v1">
+        <Header />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/honduras" element={<Carmen />} />
+          <Route path="/salvador" element={<Haydee />} />
+          <Route path="/guatemala" element={<Wendy />} />
+        </Routes>
+      </Router>
     </>
   );
 }
